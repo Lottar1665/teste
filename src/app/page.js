97449e52f7e3783bf7cbb9dafe539627e30b9024
Lottar1665 <rilottar5221@gmail.com>
2024-01-23 03:21:07 -0300

    feat/Initial styles globals

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import './globals.css'
 export default function Home() {
+
+
   return (
    <>
     <h1>Textando o deplpy </h1>
